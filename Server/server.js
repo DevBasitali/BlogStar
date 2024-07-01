1505380993b164f0d1702ad001f0e85eaeb39689
DevBasitali <rajaalix2022@gmail.com>
2024-07-01 18:38:35 +0500

    first commit

---
+++ Server/server.js
@@ -0,0 +1,17 @@
+const express = require ('express')
+const mongoose = require ('mongoose')
+const cors = require('cors')
+
+
+const app = require ('express')
+
+mongoose.connect('mongodb://localhost:27017/BlogStar')
+.then(()=> console.log('MongoDB is Connected!!!'))
+.catch(err => console.log(err))
+
+
+app.use(cors());
+app.use(express.json())
+
+
+
