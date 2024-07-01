0c6c3908873620482cc7499a1e37072572f5e6f2
DevBasitali <rajaalix2022@gmail.com>
2024-07-01 18:44:06 +0500

    commit_02

--- Server/model/User.js
+++ Server/model/User.js
@@ -8,6 +8,15 @@ const UserSchema = new Schema({
         type: String,
         required: true,
         unique: true
+    }, 
+    email: {
+        type: String,
+        required: true,
+        unique: true
+    },
+    password: {
+        type: String,
+        required: true,
     }
 })
 
