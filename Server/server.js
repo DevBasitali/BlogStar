89a240e646260fb75272c94254664406ce683c2f
DevBasitali <rajaalix2022@gmail.com>
2024-07-02 15:54:48 +0500

    02

--- Server/server.js
+++ Server/server.js
@@ -5,13 +5,19 @@ const cors = require('cors')
 
 const app = require ('express')
 
+
+app.use(cors());
+app.use(express.json())
+
 mongoose.connect('mongodb://localhost:27017/BlogStar')
 .then(()=> console.log('MongoDB is Connected!!!'))
 .catch(err => console.log(err))
 
+app.use ('/api/auth', authRoutes)
+app.use ('/api/auth', blogRoutes)
 
-app.use(cors());
-app.use(express.json())
+
+
 
 
 
