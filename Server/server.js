4d935e22d39a3872bd348212dc97157670b0ac84
DevBasitali <rajaalix2022@gmail.com>
2024-07-03 18:48:07 +0500

    wed, 03 july

--- Server/server.js
+++ Server/server.js
@@ -2,9 +2,7 @@ const express = require ('express')
 const mongoose = require ('mongoose')
 const cors = require('cors')
 
-
-const app = require ('express')
-
+const app = express();
 
 app.use(cors());
 app.use(express.json())
@@ -13,11 +11,12 @@ mongoose.connect('mongodb://localhost:27017/BlogStar')
 .then(()=> console.log('MongoDB is Connected!!!'))
 .catch(err => console.log(err))
 
-app.use ('/api/auth', authRoutes)
-app.use ('/api/auth', blogRoutes)
+// app.use ('/api/auth', authRoutes)
+// app.use ('/api/auth', blogRoutes)
 
+const PORT = process.env.PORT || 5000;
 
-
+ app.listen(PORT,()=> console.log(`Port is listening on  ${PORT}`));
 
 
 
