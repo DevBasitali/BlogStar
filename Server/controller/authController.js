b2ec48c4f9e9b831478ee6db620c30ad59e61859
DevBasitali <rajaalix2022@gmail.com>
2024-07-11 17:56:08 +0500

    Thu, 11 july 2024

--- Server/controller/authController.js
+++ Server/controller/authController.js
@@ -14,6 +14,7 @@ exports.Signup = async (req, res) => {
     }
     const hashedPassword = await bcrypt.hash(password, 10);
     const newUser = new User({ email, username, password: hashedPassword });
+
     await newUser.save();
     res.status(401).json(newUser);
   } catch (err) {
@@ -27,21 +28,47 @@ exports.Login = async (req, res) => {
   try {
     const user = await User.findOne({ email });
     if (!user) {
-      return res.status(400).json({ msg: "this user is not found" });
+      return res.status(400).json({ msg: "User not found: Signup First" });
     }
 
-    const isMatch = await bcrypt.compare(password, User.password);
+    const isMatch = await bcrypt.compare(password, user.password);
 
     if (!isMatch) {
-      return res.status(400).json({ msg: "Invalid credentials" });
+      return res.status(400).json({ msg: 'Invalid credentials' });
     }
 
-    // const payload = { id: User.id, username: User.username };
-    // jwt.sign(payload, config.jwtKey, { expiresIn: 3600 }, (err, token) => {
-    //   if (err) throw err;
-    //   res.json({ token });
-    // });
+    const payload = { id: user._id }; 
+    const token = jwt.sign(payload, config.jwtKey, { expiresIn: 3600 }); 
+
+    res.status(200).json({ token, msg: "Login successful" }); 
+
   } catch (err) {
-    res.status(500).json({ msg: "Server error" });
+    res.status(500).json({ msg: "Something is Wrong" });
   }
 };
+
+
+// exports.Login = async (req, res) => {
+//   const { email, password } = req.body;
+
+//   try {
+//     const user = await User.findOne({ email });
+//     if (!user) {
+//       return res.status(400).json({ msg: "User not found: Signup First" });
+//     }
+
+//     const isMatch = await bcrypt.compare(password, user.password);
+//     if (!isMatch) {
+//       return res.status(400).json({ msg: "Invalid credentials" });
+//     }
+
+//     const payload = { id: User.id, username: User.username };
+//     jwt.sign(payload, config.jwtKey, { expiresIn: 3600 }, (err, token) => {
+//       if (err) throw err;
+//       res.json({ token });
+//     });
+
+//   } catch (err) {
+//     res.status(500).json({ msg: "Something is Wrong" });
+//   }
+// };
--- Server/routes/authRoutes.js
+++ Server/routes/authRoutes.js
@@ -1,8 +1,6 @@
 const express = require('express');
 const router = express.Router();
 const authController = require('../controller/authController');
-// const authController = require('../controller/blogController');
-
 
 router.post('/Signup', authController.Signup);
 router.post('/Login', authController.Login);
