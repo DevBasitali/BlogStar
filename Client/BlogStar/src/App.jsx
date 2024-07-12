553f5fbcc63ab9cce6ef96dca62811b860549453
DevBasitali <rajaalix2022@gmail.com>
2024-07-12 18:07:45 +0500

    Fri, 12 July 2024

--- Client/BlogStar/src/App.jsx
+++ Client/BlogStar/src/App.jsx
@@ -1,14 +1,12 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
   return (
     <>
-    <h1 className='font-mono text-black bg-slate-900 '>
-      hello world!!!
-    </h1>
+      <h1 className="bg-black text-white">hello pakistan </h1>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
+
